125b66ba84da2510526cd720a663e5664cfe8a0c
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-08 23:33:07 -0300

    chore: fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,47 +2,45 @@ require 'spec_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'Get /index' do
-
     before :each do
       get '/users/10/posts'
     end
 
-    it "Response status when requesting => :ok" do
+    it 'Response status when requesting => :ok' do
       expect(response).to have_http_status(:ok)
     end
-  
-    it "Render the correct template" do
+
+    it 'Render the correct template' do
       expect(response).to render_template(:index)
     end
 
-    it "Render the correct placeholder text" do
-      expect(response.body).to include("LISTING ALL POSTS FOR A USER")
+    it 'Render the correct placeholder text' do
+      expect(response.body).to include('LISTING ALL POSTS FOR A USER')
     end
 
-    it "does not render a different template" do
+    it 'does not render a different template' do
       expect(response).to_not render_template(:show)
     end
   end
 
   describe 'Get /show' do
-
     before :each do
       get '/users/10/posts/10'
     end
 
-    it "Response status when requesting => :ok" do
+    it 'Response status when requesting => :ok' do
       expect(response).to have_http_status(:ok)
     end
-  
-    it "Render the correct template" do
+
+    it 'Render the correct template' do
       expect(response).to render_template(:show)
     end
 
-    it "Render the correct placeholder text" do
+    it 'Render the correct placeholder text' do
       expect(response.body).to include("SHOW POST'S FOR A USER")
     end
 
-    it "does not render a different template" do
+    it 'does not render a different template' do
       expect(response).to_not render_template(:index)
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,48 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users', type: :request do
   describe 'Get /index' do
-
     before :each do
       get '/users'
     end
 
-    it "Response status when requesting => :ok" do
+    it 'Response status when requesting => :ok' do
       expect(response).to have_http_status(:ok)
     end
-  
-    it "Render the correct template" do
+
+    it 'Render the correct template' do
       expect(response).to render_template(:index)
     end
 
-    it "Render the correct placeholder text" do
-      expect(response.body).to include("LISTING ALL USERS")
+    it 'Render the correct placeholder text' do
+      expect(response.body).to include('LISTING ALL USERS')
     end
 
-    it "does not render a different template" do
+    it 'does not render a different template' do
       expect(response).to_not render_template(:show)
     end
   end
 
   describe 'Get /show' do
-
     before :each do
       get '/users/10/'
     end
 
-    it "Response status when requesting => :ok" do
+    it 'Response status when requesting => :ok' do
       expect(response).to have_http_status(:ok)
     end
-  
-    it "Render the correct template" do
+
+    it 'Render the correct template' do
       expect(response).to render_template(:show)
     end
 
-    it "Render the correct placeholder text" do
-      expect(response.body).to include("DISPLAY A USER FULL INFORMATION")
+    it 'Render the correct placeholder text' do
+      expect(response.body).to include('DISPLAY A USER FULL INFORMATION')
     end
 
-    it "does not render a different template" do
+    it 'does not render a different template' do
       expect(response).to_not render_template(:index)
     end
   end
