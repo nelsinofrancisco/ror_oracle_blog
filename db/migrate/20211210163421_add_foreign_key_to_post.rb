206a75327f1b60e3d79da8571235b79b40e9dd99
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-10 13:44:33 -0300

    feature: change fk column name for all references to users id

--- db/migrate/20211210163421_add_foreign_key_to_post.rb
+++ db/migrate/20211210163421_add_foreign_key_to_post.rb
@@ -1,5 +1,5 @@
 class AddForeignKeyToPost < ActiveRecord::Migration[6.1]
   def change
-    add_reference :posts, :author_id, null: false, foreign_key: { to_table: :users }
+    add_reference :posts, :author, null: false, foreign_key: { to_table: :users }
   end
 end
--- db/migrate/20211210163735_add_foreign_key_to_comment.rb
+++ db/migrate/20211210163735_add_foreign_key_to_comment.rb
@@ -1,6 +1,6 @@
 class AddForeignKeyToComment < ActiveRecord::Migration[6.1]
   def change
-    add_reference :comments, :author_id, null: false, foreign_key: { to_table: :users }
+    add_reference :comments, :author, null: false, foreign_key: { to_table: :users }
     add_reference :comments, :post, null: false, foreign_key: true
   end
 end
