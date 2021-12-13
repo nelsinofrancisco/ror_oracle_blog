f5667696c4846a5765fb0b961b91e708da3be9f5
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-13 17:03:51 -0300

    feature: add methods for updating posts count for a user and to return recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,15 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+
+  def update_posts_count
+    user = User.find(author_id)
+    posts_count = Posts.where(author_id: author_id).count
+    user.update(posts_counter: posts_count)
+  end
+
+  def recent_comments
+    comments = Comment.where(post_id: id)
+    comments.order(created_at: :desc).limit(5)
+  end
 end
