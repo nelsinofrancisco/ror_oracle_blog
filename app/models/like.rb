c6c8789bbd2907bf5cf473902929f14a027d4fa8
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-13 17:19:18 -0300

    feature: add method to update likes count in Like Model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :users, foreign_key: => :author_id
   belongs_to :posts
+
+  def update_likes_count
+    post = Post.find(self.post_id)
+    likes_count = Like.where(post_id: self.post_id).count
+    post.update(likes_count: likes_count)
+  end
 end
