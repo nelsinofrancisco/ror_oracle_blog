64d5fbdbbda233b0f73a266c1b80d8a648cc32c9
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-13 17:15:15 -0300

    feature: add method to update comments count in Comment  model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :users, foreign_key: => :author_id
   belongs_to :posts
+
+  def update_comments_count
+    post = Post.find(self.post_id)
+    comments_count = Posts.where(id: self.post_id).count
+    post.update(comments_count: comments_count)
+  end
 end
