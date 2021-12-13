19f64fa21e0baa50a66d2a8486ace0ac2c34f750
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-13 16:39:48 -0300

    feature: setup associations one-to-many for likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,4 @@
 class Like < ApplicationRecord
+  belongs_to :users, foreign_key: => :author_id
+  belongs_to :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :users, foreign_key: author_id
+  belongs_to :users, foreign_key: => :author_id
   has_many :likes
   has_many :comments
-  
+
 end
