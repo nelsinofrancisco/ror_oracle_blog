8d992c74c42f0adb34f5e53b420abf0964ee9539
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-14 04:03:49 -0300

    feature: fix belongs_to syntax and remove redundant queries

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,8 @@
 class Like < ApplicationRecord
-  belongs_to :users, foreign_key: :author_id, inverse_of: :likes
-  belongs_to :posts, inverse_of: :likes
+  belongs_to :user, foreign_key: :author_id, inverse_of: :likes
+  belongs_to :post, inverse_of: :likes
 
   def update_likes_count
-    post = Post.find(post_id)
-    likes_count = Like.where(post_id: post_id).count
-    post.update(likes_counter: likes_count)
+    post.update(likes_counter: post.likes.count)
   end
 end
