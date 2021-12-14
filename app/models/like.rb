32836d1fd9e6f783e9134d1d3f11a7e2bea9348d
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-14 15:48:39 -0300

    feature: add after_save callback to update_likes_count

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :user, foreign_key: :author_id, inverse_of: :likes
   belongs_to :post, inverse_of: :likes
+  after_save :update_likes_count
 
   def update_likes_count
-    post.update(likes_counter: post.likes.count)
+    post.increment!(:likes_counter)
   end
 end
