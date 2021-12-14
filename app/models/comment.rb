46f6f724a5a9704c4b713a45ef2826cc11392b1e
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-14 04:03:22 -0300

    feature: fix belongs_to syntax and remove redundant queries

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :users, foreign_key: :author_id, inverse_of: :comments
-  belongs_to :posts, inverse_of: :comments
+  belongs_to :user, foreign_key: :author_id, inverse_of: :comments
+  belongs_to :post, inverse_of: :comments
 
   def update_comments_count
-    post = Post.find(post_id)
-    comments_count = Comment.where(post_id: post_id).count
-    post.update(comments_counter: comments_count)
+    post.update(comments_counter: post.comments.count)
   end
 end
