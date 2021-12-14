ae7123e5e6f0a61dd6033e9836324bd03ad004e6
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-14 04:01:58 -0300

    feature: fix inverse_of syntax and remove redundant query

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id, inverse_of: :users
-  has_many :likes, foreign_key: :author_id, inverse_of: :users
-  has_many :comments, foreign_key: :author_id, inverse_of: :users
+  has_many :posts, foreign_key: :author_id, inverse_of: :user
+  has_many :likes, foreign_key: :author_id, inverse_of: :user
+  has_many :comments, foreign_key: :author_id, inverse_of: :user
 
   def recent_posts
-    posts = Post.where(author_id: id)
     posts.order(created_at: :desc).limit(3)
   end
 end
