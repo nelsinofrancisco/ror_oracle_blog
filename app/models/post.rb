8e06c909cc2467ea07427d36298fabc877f71073
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-14 04:02:45 -0300

    feature: fix belongs_to syntax and remove redundant syntax

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,13 @@
 class Post < ApplicationRecord
-  belongs_to :users, foreign_key: :author_id, inverse_of: :posts
+  belongs_to :user, foreign_key: :author_id, inverse_of: :posts
   has_many :likes
   has_many :comments
 
   def update_posts_count
-    user = User.find(author_id)
-    posts_count = Posts.where(author_id: author_id).count
-    user.update(posts_counter: posts_count)
+    user.update(posts_counter: user.posts.count)
   end
 
   def recent_comments
-    comments = Comment.where(post_id: id)
     comments.order(created_at: :desc).limit(5)
   end
 end
