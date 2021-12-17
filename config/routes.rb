9bf7dd271982eb9be47a8bc00f85a53c9f96d97e
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-17 19:33:04 -0300

    feature: add extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users do
+
+  # resources :comments, only: [:create], path: '/users/:id/posts'
+
+  resources :posts, only: [:create], path: '/users/:user_id/posts/:id'
+
+  resources :users, only: [:index, :show] do
     resources :posts
   end
+
+  resources :users, only: [:create, :new], path: '/users/:id'
   root to: "users#index"
 end
