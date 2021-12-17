f73fb2cf9947233e78dbb8e5f6a6d3e925ee45ba
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-17 10:47:37 -0300

    feature: change routes and link_to paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/users/:user_id/posts/:id", to: "posts#show", as: 'users_posts'
-  get "/users/:user_id/posts", to: "posts#index", as: 'user_post'
-  get "/users/:id", to: "users#show", as: 'users'
-  get "/users", to: "users#index", as: 'user'
+  resources :users do
+    resources :posts
+  end
   root to: "users#index"
 end
