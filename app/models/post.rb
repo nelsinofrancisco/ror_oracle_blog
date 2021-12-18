98d1c6cb325cdcb0cbf751921e026b59e5c2b435
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-18 16:46:45 -0300

    feature: add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ApplicationRecord
   has_many :comments
   after_save :update_posts_count
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
