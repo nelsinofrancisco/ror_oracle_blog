1f74d915f3beccacafedd12226fd3a508cb9eebc
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-26 14:39:35 -0300

    feature: add authenticate_user! tobefore  action hook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  
+  protected
 
-  def current_user
-    @current_user = User.first
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :posts_counter])
   end
 end
