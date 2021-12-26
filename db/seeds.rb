50695613ac5860d3300453d0f15bee4e05b0158b
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-26 14:43:07 -0300

    feature: update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,28 +13,34 @@ text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus s
 Ut at libero consectetur, finibus augue eu, feugiat mauris. Integer sollicitudin orci ex, facilisis tempor mauris tristique id. Donec et tristique est, eget tempus justo. Integer lacus nibh, sollicitudin imperdiet tempor vitae, dictum et tellus. Suspendisse in arcu mattis eros lacinia blandit. Donec a sapien felis. Aenean ornare interdum risus eget molestie. Nulla facilisis lacus non lacus bibendum, id posuere augue porttitor. Nullam sodales enim sem, et ultricies lorem hendrerit at. Mauris placerat dictum sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus venenatis nisl sed blandit."
 
 
-users = User.create([
-  { name: 'Nelsino', photo: 'Cool_Photo_1', bio: "I'm the last oracle", posts_counter: 0},
-  { name: 'Hamza', photo: 'Cool_Photo_1', bio: "I'm the First oracle", posts_counter: 0},
-  { name: 'Juan', photo: 'Cool_Photo_1', bio: "I'm the Best oracle", posts_counter: 0},
-  { name: 'Mateo', photo: 'Cool_Photo_1', bio: "I'm the Crazy oracle", posts_counter: 0},
-  { name: 'Laylo', photo: 'Cool_Photo_1', bio: "I'm the sleepy oracle", posts_counter: 0},
-  { name: 'Herbert', photo: 'Cool_Photo_1', bio: "I'm the rider oracle", posts_counter: 0}
-  ])
+names = [
+  { name: 'Nelsino', bio: "I'm the last oracle", posts_counter: 0, email:'foo1@foo.com', role: 'admin'},
+  { name: 'Hamza', bio: "I'm the First oracle", posts_counter: 0, email:'foo2@foo.com', role: 'admin'},
+  { name: 'Herbert', bio: "I'm the Best oracle", posts_counter: 0, email:'foo3@foo.com', role: 'admin'},
+  { name: 'Laylo', bio: "I'm the sleep oracle", posts_counter: 0, email:'foo4@foo.com', role: 'user'},
+  { name: 'Juan', bio: "I'm the bird oracle", posts_counter: 0, email:'foo5@foo.com', role: 'user'}
+  ]
+  
+for i in 0..4 do
+  user = User.new(names[i])
+  user.password = 'admin123'
+  user.password_confirmation = 'admin123'
+  user.save
+end
 
 all_users = User.all
 
 all_users.each do |user|
   for j in 1..5 do
-    Post.create(title: "Post ##{j}", text: text, comments_counter: 0, likes_counter: 0, author_id: user.id)
+    Post.create(title: "Awsome post", text: "I'm writing my post number: #{j}", comments_counter: 0, likes_counter: 0, author_id: user.id)
   end
 end
 
 all_posts = Post.all
 
 all_posts.each do |post|
-  for j in 1..5 do
+  for j in 0..4 do
     Like.create(author_id: all_users[j].id, post_id: post.id)
     Comment.create(author_id: all_users[j].id, post_id: post.id, text: "I'm #{all_users[j].name} and I'm commenting gibberish here.")
   end
-end
+end 
