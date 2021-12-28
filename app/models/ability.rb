1883bad7841162d0924bf54a4c513f973663ea66
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-28 19:32:22 -0300

    feature: change permissions for user role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,15 +4,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, User, user: user
-    can :read, Post, user: user
-    can :read, Comment, user: user
-    can :read, Like, user: user
+    can :read, :all
 
     return unless user.present? # additional permissions for logged in users
 
+    can :create, Comment
+    can :create, Like
+    can :create, Post
     can %i[update destroy], Post, author_id: user.id
     can %i[update destroy], Comment, author_id: user.id
+    
 
     return unless user.is? :admin # additional permissions for administrators
 
