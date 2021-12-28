f3693fa469f05ab257dc0a8ae11dc8a691cc5a2f
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-12-28 15:13:35 -0300

    feature: add function to decrement posts and comments counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   after_save :update_posts_count
+  after_destroy :decrement_posts_count
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -16,5 +17,9 @@ class Post < ApplicationRecord
 
   def update_posts_count
     user.increment!(:posts_counter)
+  end
+
+  def decrement_posts_count
+    user.decrement!(:posts_counter)
   end
 end
